4cc888fc3f4e43557d47d76a87c46606a2636d60
Jacek programmer <jacek301@gmail.com>
2024-07-21 00:29:48 +0200

    Ordered Count of Characters kata solution

--- src/main/groovy/Kata.groovy
+++ src/main/groovy/Kata.groovy
@@ -1,5 +1,14 @@
 class Kata {
-    static def orderedCount(input) {
-        // Implement me! :)
+    static def orderedCount(String input) {
+
+        def signList = input.split('').toList()
+
+        def singCountMap = [:]
+        signList.forEach { sign ->
+            singCountMap.put(sign, input.count(sign))
+        }
+
+        return singCountMap.collect {key , value -> new Tuple(key as String,value)}
+
     }
 }
