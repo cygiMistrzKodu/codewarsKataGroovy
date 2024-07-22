15c4b83dd1b943594d682b3c12fb460021834b72
Jacek programmer <jacek301@gmail.com>
2024-07-22 22:23:48 +0200

    Odd or Even? kata solution

--- src/main/groovy/Kata.groovy
+++ src/main/groovy/Kata.groovy
@@ -1,6 +1,11 @@
-class Kata{
-    static String oddOrEven(list){
+class Kata {
+    static String oddOrEven(List<Integer> list) {
 
+        if (list.isEmpty()) {
+            return 'even'
+        }
+
+        list.sum() % 2 == 0 ? 'even' : 'odd'
 
     }
 }
