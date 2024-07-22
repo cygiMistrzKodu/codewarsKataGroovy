215c5a896b07bcd60ac683a972b7fd88522a1dfc
Jacek programmer <jacek301@gmail.com>
2024-07-22 23:34:27 +0200

    Chords kata solution

--- src/main/groovy/Kata.groovy
+++ src/main/groovy/Kata.groovy
@@ -1,5 +1,28 @@
 class Kata{
     public static final notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-    static chords(root){
+    static chords(String root){
+
+        def rootNotePosition = notes.indexOf(root)
+
+        def secondMajorSemitone = foundSemitonePosition(rootNotePosition, 4)
+        def secondMinorSemitone = foundSemitonePosition(rootNotePosition, 3)
+        def thirdSemitone = foundSemitonePosition(rootNotePosition, 7)
+
+
+        def majorChord = [root,notes[secondMajorSemitone] , notes[thirdSemitone]]
+        def minorChord = [root,notes[secondMinorSemitone] , notes[thirdSemitone]]
+
+        [majorChord,minorChord]
+
+
+    }
+
+    private static int foundSemitonePosition(int rootNotePosition, int semitonePositionFromRoot) {
+        def searchSemitonePosition = rootNotePosition + semitonePositionFromRoot
+
+        if (searchSemitonePosition >= notes.size()) {
+            searchSemitonePosition = searchSemitonePosition -  notes.size()
+        }
+        searchSemitonePosition
     }
 }
