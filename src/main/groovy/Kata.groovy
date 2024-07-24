1ed8563cd7ea423b6a218b5b6a9df27f7acb1f1a
Jacek programmer <jacek301@gmail.com>
2024-07-24 16:35:31 +0200

    Hungarian Vowel Harmony (easy) kata solution

--- src/main/groovy/Kata.groovy
+++ src/main/groovy/Kata.groovy
@@ -1,5 +1,25 @@
 class Kata {
-    static def dative(word) {
-        // Implement me! :)
+    static def dative(String word) {
+
+        def frontVowels = ["e", "é", "i", "í", "ö", "ő", "ü", "ű"]
+        def backVowels = ["a", "á", "o", "ó", "u", "ú"]
+
+        def result = ""
+
+        frontVowels.forEach {
+
+            if (word.contains(it)) {
+                result = word.concat("nek")
+            }
+        }
+
+        backVowels.forEach {
+
+            if (word.contains(it)) {
+                result = word.concat("nak")
+            }
+        }
+
+        result
     }
 }
