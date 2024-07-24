ea68c97b80adba0c1643ea1773128138a0849de9
Jacek programmer <jacek301@gmail.com>
2024-07-24 16:41:59 +0200

    Remove B M W kata start

--- src/main/groovy/Kata.groovy
+++ src/main/groovy/Kata.groovy
@@ -1,25 +1,5 @@
-class Kata {
-    static def dative(String word) {
-
-        def frontVowels = ["e", "é", "i", "í", "ö", "ő", "ü", "ű"]
-        def backVowels = ["a", "á", "o", "ó", "u", "ú"]
-
-        def result = ""
-
-        frontVowels.forEach {
-
-            if (word.contains(it)) {
-                result = word.concat("nek")
-            }
-        }
-
-        backVowels.forEach {
-
-            if (word.contains(it)) {
-                result = word.concat("nak")
-            }
-        }
-
-        result
+class BMWRemover {
+    static removeBMW(str) {
+        // code here
     }
 }
--- src/test/groovy/TestExample.groovy
+++ src/test/groovy/TestExample.groovy
@@ -1,16 +1,10 @@
 import org.junit.jupiter.api.Test
 
-class ExampleTets {
+
+class TestExample {
     @Test
-    void "Example Tests"() {
-        assert Kata.dative("ablak") == "ablaknak"
-        assert Kata.dative("tükör") == "tükörnek"
-        assert Kata.dative("keret") == "keretnek"
-        assert Kata.dative("otthon") == "otthonnak"
-        assert Kata.dative("virág") == "virágnak"
-        assert Kata.dative("tett") == "tettnek"
-        assert Kata.dative("rokkant") == "rokkantnak"
-        assert Kata.dative("rossz") == "rossznak"
-        assert Kata.dative("gonosz") == "gonosznak"
+    void "Basic tests"() {
+        assert BMWRemover.removeBMW("bmwvolvoBMW") == "volvo"
+        assert BMWRemover.removeBMW("blablahblah") == "lalahlah"
     }
 }
